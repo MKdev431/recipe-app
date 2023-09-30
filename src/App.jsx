f247510e8b9fcca44410aefc92ba0c258966074b
Michal Kurek (Mike) <michal@kurek.pl>
2023-09-30 13:29:23 +0200

    add logo and framer-motion effect = fade in/out

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Category from "./components/Category";
 import Pages from "./pages/Pages";
 import { BrowserRouter as Router } from "react-router-dom";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
 
 import "./App.css";
 
@@ -9,6 +12,10 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>Deliciousss</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -16,5 +23,23 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-size: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+    cursor: pointer;
+  }
+`;
 
 export default App;
